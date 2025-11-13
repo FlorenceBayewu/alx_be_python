3270298f1ef926c62e0cbb24c52c956737b3043a
Florence Bayewu <florencebayewu2020@gmail.com>
2025-11-13 18:01:07 +0100

    Save changes made to daily reminder file in script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,20 @@
-task = input("Input a task description: ")
-task_priority = input("What's the task priority?(high,medium,low) " )
-time_bound = input("Is task time-bound? ")
-match task_priority:
+Task = input("Input a task description: ")
+Priority = input("What's the task priority?(high,medium,low) " )
+Time_bound = input("Is task time-bound? ")
+match Priority:
     case "high":
-        if time_bound == "yes":
-            reminder = f"{task} is a high priority task that requires immediate attention"
+        if Time_bound == "yes":
+            reminder = f"{Task} is a high priority task that requires immediate attention"
         else:
-            reminder = f"{task} is a high priority task"
+            reminder = f"{Task} is a high priority task"
     case "medium":
-        if time_bound == "yes":
-            reminder = f"{task} is a medium priority task that requires immediate attention"
+        if Time_bound == "yes":
+            reminder = f"{Task} is a medium priority task that requires immediate attention"
         else:
-            reminder = f"{task} is a medium priority task"
+            reminder = f"{Task} is a medium priority task"
     case "low":
-        if time_bound == "yes":
-            reminder = f"{task} is a low priority task that requires immediate attention"
+        if Time_bound == "yes":
+            reminder = f"{Task} is a low priority task that requires immediate attention"
         else:
-            reminder = f"{task} is a low priority task"
+            reminder = f"{Task} is a low priority task"
 print(f"Reminder: {reminder}")
