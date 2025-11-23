3a6324f17654bcc81e341220f7fbc873c9ea4308
Florence Bayewu <florencebayewu2020@gmail.com>
2025-11-23 19:15:33 +0100

    save change

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation=="multiply":
         return num1*num2
     elif operation=="divide":
-        if num2==0:
-            return ("Error: Cannot divide by 0")
-        return num1/num2
+        if num2 == 0:
+            return("Error: Cannot divide by 0")
+        else:
+         return num1/num2
     else:
-        return "Invalid operation."
+        return "Error: Invalid operation."
